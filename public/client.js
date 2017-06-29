8d7e0441bb44a28f4777670e3b0ca47bfee8fe63
Oskar <oskarrough@gmail.com>
2017-06-29 21:42:01 +0200

    Add usernames (#4)
    * Lint and upgrade prettier
    
    * Upgrade socket.io
    
    * Add usernames
    
    * Move template inline
    
    * Change `yarn lint` to `yarn format`, which is what it does
    
    * Styling
    
    * Update username UX
    
    * Update UX

--- public/client.js
+++ public/client.js
@@ -1,18 +1,42 @@
 /* global io, Vue */
 
+var template = `
+<div class="RoughChat">
+	<p v-if="users > 1" class="tr">Online: <span v-cloak>{{users}}</span></p>
+	<ol v-cloak>
+		<li v-for="message in messages">
+			<strong v-if="message.username">{{message.username}}</strong>
+			<strong v-else>Anonymous</strong>: {{message.content}}
+		</li>
+	</ol>
+	<form class="df" v-if="!username" v-on:submit.prevent="setUsername">
+		<input autofocus v-model="usernameInput" class="f-1" placeholder="Hi, what may we call you?" title="What is your name?" />
+		<button type="submit">OK</button>
+	</form>
+	<form v-else class="row" v-on:submit.prevent="sendMessage">
+		<button v-on:click="changeUsername" title="Tap here to change your name" class="reset-btn" type="button">{{username}}</button>
+		<input v-model="message" placeholder="Send a message" class="f-1 ml-05" autofocus autocomplete="off" />
+		<button type="submit">Chat</button>
+	</form>
+</div>
+`
+
 var chat = new Vue({
 	el: '#chat',
-	data: function () {
+	template: template,
+	data: function() {
 		return {
-			users: 0,
+			usernameInput: '',
+			username: '',
 			message: '',
-			messages: []
+			messages: [],
+			users: 0
 		}
 	},
-	mounted: function () {
+	mounted: function() {
 		// Set up the socket connection with dynamic host.
 		var host = ''
-		var attrs  = this.$el.attributes
+		var attrs = this.$el.attributes
 		if (attrs.host) {
 			host = attrs.host.value
 		}
@@ -20,8 +44,8 @@ var chat = new Vue({
 
 		// Listen to socket events.
 		this.socket
-			.on('chat message', msg => {
-				this.$data.messages.push({text: msg})
+			.on('chat message', message => {
+				this.$data.messages.push(message)
 			})
 			.on('update users', users => {
 				this.$data.users = users
@@ -29,11 +53,25 @@ var chat = new Vue({
 	},
 	methods: {
 		sendMessage() {
-			var msg = this.$data.message
-			this.socket.emit('chat message', msg)
-			this.$data.messages.push({text: msg})
+			if (!this.$data.message.length) return
+			var message = {
+				username: this.$data.username,
+				content: this.$data.message
+			}
+			this.socket.emit('chat message', message)
+			this.$data.messages.push(message)
 			this.$data.message = ''
+		},
+		setUsername() {
+			this.username = this.usernameInput
+		},
+		changeUsername() {
+			this.username = ''
+			this.$nextTick(() => {
+				var el = this.$el.querySelector('input')
+				el.focus()
+				el.select()
+			})
 		}
 	}
 })
-
