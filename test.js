d2c018ace510f0f790538261aae739f227f33735
Oskar Rough <oskar@rough.dk>
2017-08-01 08:49:09 +0200

    Clean up

--- test.js
+++ test.js
@@ -1,22 +1,22 @@
 import Nightmare from 'nightmare'
 import test from 'ava'
 
-const server = (options) => {
+const server = options => {
 	const nightmare = Nightmare(options)
 	return nightmare.goto('http://localhost:3000')
 }
 
 test('can set username and post a message', async t => {
-	const result = await server()
+	const message = await server({show:true})
 		.type('.RoughChat input', 'Donald Byrd')
 		.click('.RoughChat button')
 		.wait(100)
 		.type('.RoughChat input', 'it grooves')
 		.click('.RoughChat button[type="submit"]')
+		.wait(100)
 		.evaluate(function() {
-			var els = document.querySelectorAll('.RoughChat ol li')
-			return els[0].innerText
+			return document.querySelectorAll('.RoughChat ol li')[0].innerText
 		})
 		.end()
-	t.is(await result, 'Donald Byrd: it grooves')
+	t.is(message, 'Donald Byrd: it grooves')
 })
